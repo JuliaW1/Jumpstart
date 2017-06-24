76815e8efc805d73f2afe7b0f7c5e597f11e8404
Julia Wannamaker <juliawannamaker@gmail.com>
2017-06-24 16:46:03 -0400

    Add CSS and Files Final Code Bits

---
+++ resources/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    slidesToScroll: 1,
+    draggable: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    autoplaySpeed: 4000,
+    arrows: false // Get rid of that pesky horizontal scrollbar
+  });
+
+
+// https://api.jquery.com/scroll/
+// https://api.jquery.com/animate/
+
+  var scrolledFromTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - scrolledFromTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrolledFromTop = scrollTop;
+    } else if (scrolledFromTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      scrolledFromTop = scrollTop;
+    }
+  });
+});
